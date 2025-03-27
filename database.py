91403fa97fd4d04e95fa12c83d75e2c022753c57
Ezequiel-Portifolio <ezequiel.alves@aluno.ifsertao-pe.edu.br>
2025-03-27 15:48:24 -0300

    Add functionality to save and load movies from a JSON file Prepare for "movie deletion" feature

--- database.py
+++ database.py
@@ -11,3 +11,35 @@ filmes_cadastrados = {}
       }
 }                        
 """ 
+
+def salvar_arquivoFilmes(): #Salva o dicionário de filmes cadastrados em um arquivo JSON
+  try:
+    #abre ou cria o arquivo, se não existir
+    with open("dados_filmes.json", "w", encoding="utf-8") as arquivo: 
+      #Salva o dicionário no arquivo JSON
+      json.dump(filmes_cadastrados, arquivo, indent=4, ensure_ascii=False)
+    print ("Arquivo de filmes salvo com sucesso em dados_filmes.json")
+    return True
+  except Exception as erro:
+    print (f"Erro ao salvar o arquivo de filmes: {erro}")
+    return False
+
+def carregar_arquivoFilmes(): #Carrega o dicionário de filmes cadastrados salvo no JSON
+  global filmes_cadastrados
+  
+  try:
+    #Tentar abrir o arquivo para leitura
+    with open("dados_filmes.json", "r", encoding="utf-8") as arquivo:
+      #Carrega o dicionário do arquivo JSON
+      filmes_cadastrados = json.load(arquivo)
+    print(f"{len(filmes_cadastrados)} filmes carregados com sucesso.")
+    return True
+  
+  except FileNotFoundError:
+    # Se o arquivo não existir, não carrega nada
+    print("Arquivo de filmes não encontrado. Verifique se o arquivo está no mesmo diretório.")
+    return False
+  
+  except Exception as erro:
+    print(f"Erro ao carregar o arquivo de filmes: {erro}")
+    return False
--- main.py
+++ main.py
@@ -6,7 +6,23 @@ import time
 
 def limpar_console():
   os.system('cls' if os.name == 'nt' else 'clear') #Limpa o console
+
+def SalvarFilmes():
+  database.salvar_arquivoFilmes()
+  entrada = input("Pressione qualquer tecla para continuar")
+  print ("Carregando...")
+  time.sleep(1)
+  limpar_console()
+  MenuPrincipal()
   
+def CarregarFilmes():
+  database.carregar_arquivoFilmes()
+  entrada = input("Pressione qualquer tecla para continuar")
+  print ("Carregando...")
+  time.sleep(1)
+  limpar_console()
+  MenuPrincipal()
+
 def OpcoesMenu(ValorOpcao):
   if ValorOpcao == 0: #Se a opção for 0, fecha o console
     print ("Saindo do sistema...")
@@ -33,7 +49,7 @@ def OpcoesMenu(ValorOpcao):
     MenuPrincipal()
     
   elif ValorOpcao == 2: #Se a opção for 2, mostra os filmes cadastrados
-    if database.filmes_cadastrados:
+    if database.filmes_cadastrados: #Se houver filmes cadastrados, mostra os filmes
      for filme in database.filmes_cadastrados:
       print ("ID do filme:", filme)
       print ("Nome do filme:", database.filmes_cadastrados[filme]["Nome"])
@@ -41,7 +57,7 @@ def OpcoesMenu(ValorOpcao):
       print ("Gênero do filme:", database.filmes_cadastrados[filme]["Genero"])
       print ("Disponibilidade do filme:", database.filmes_cadastrados[filme]["Disponibilidade"])
       print ("-----------------------------")
-    else:
+    else: #Se não houver filmes cadastrados, mostra uma mensagem de erro
       print ("Não há filmes cadastrados.")
     continuar = input("Aperte qualquer tecla para sair")
     print ("Carregando...")
@@ -49,16 +65,26 @@ def OpcoesMenu(ValorOpcao):
     limpar_console()
     MenuPrincipal()
 
+  elif ValorOpcao == 4: #Se 4, salva os filmes cadastrados em um arquivo JSON
+    SalvarFilmes()
+  
+  elif ValorOpcao == 5: #Se 5, carrega os filmes cadastrados de um arquivo JSON
+    CarregarFilmes()
+    
 def MenuPrincipal(): #Abre o menu de opções
   print ("-----------------------------")
   print ("Seja bem-vindo ao Sistema de Locadora de Vídeo.")
   print ("0 - Sair")
   print ("1 - Cadastrar Filme")
   print ("2 - Carregar Filme")
+  print ("3 - Remover Filme (Não implementado)")
+  print ("4 - Salvar Lista de Filmes em JSON")
+  print ("5 - Carregar Lista de Filmes de JSON")
+  print ("-----------------------------")
 
   try: #Tenta pegar a opção do usuário
     opcao = int(input("Digite a opção desejada: "))
-    if opcao not in [0, 1, 2]: #Se a opção não for 0, 1 ou 2, mostra uma mensagem de erro
+    if opcao not in [0, 1, 2, 4, 5]: #Se a opção não for 0, 1 ou 2, mostra uma mensagem de erro
         print("Opção inválida!")
         time.sleep(1)
         limpar_console()
